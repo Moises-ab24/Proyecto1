bfa6ff0287d02c9892ea672a8425935842d31bf2
Moises-ab24 <moi24ad@gmail.com>
2024-06-03 13:53:52 -0600

    Estructuras condicionales anidadas

---
+++ src/Estructuras_Condicionales_Anidadas.kt
@@ -0,0 +1,78 @@
+fun main (){
+        /*
+        Problema 1
+    Confeccionar un programa que pida por teclado tres notas de un alumno,
+    calcule el promedio e imprima alguno de estos mensajes:
+    Si el promedio es >=7 mostrar "Promocionado"
+    Si el promedio es >=4 y <7 mostrar "Regular"
+    Si el promedio es <4 mostrar "Reprobado"
+        */
+    /*
+    print("Ingrese primer nota:")
+    val nota1: Int = readln().toInt()
+    print("Ingrese segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("Ingrese tercera nota:")
+    val nota3: Int = readln().toInt()
+    val promedio = (nota1+nota2+nota3)/3
+    if(promedio >= 7)
+        print("Promocionado")
+    else
+        if(promedio >=4)
+            print("Regular")
+        else
+            print("Reprobado")
+     */
+    /*
+    print("Ingrese primer nota:")
+    val nota1: Int = readln().toInt()
+    print("Ingrese segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("Ingrese tercera nota:")
+    val nota3: Int = readln().toInt()
+    val promedio = (nota1+nota2+nota3)/3
+
+    val estado = if(promedio >= 7) "Promocionado" else if(promedio < 4) "Regular" else "Reprobado"
+    print("Estado del alumno:$estado")
+     */
+
+    /*
+    Problema propuesto 1
+    Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de
+    ellos.
+     */
+
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    print("Ingrese tercer valor:")
+    val valor3 = readln().toInt()
+
+    if (valor1 > valor2)
+        print("El mayor valor es $valor1")
+    else
+        if (valor2 > valor3)
+        print("El mayor valor es $valor2")
+    else
+        print("El mayor valor es $valor3")
+
+    /*
+    Problema propuesto 3
+    Confeccionar un programa que permita cargar un número entero positivo de
+    hasta tres cifras y muestre un mensaje indicando si tiene 1, 2, o 3 cifras.
+    Mostrar un mensaje de error si el número de cifras es mayor.
+     */
+    /*
+    print("Ingrese un numero del 1 al 999:")
+    val num: Int = readln().toInt()
+    if(num > 99)
+        print("El numero tiene 3 cifras")
+    else
+        if(num > 9)
+            print("El numero tiene 2 cifras")
+        else
+            print("El numero tiene 1 cifra")
+    */
+
+}
